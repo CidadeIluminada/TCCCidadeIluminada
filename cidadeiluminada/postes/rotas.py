1a6c936120371e5415b32be41fddeba5facff4b6
Arthur Bressan <arthurpbressan@gmail.com>
2015-09-19 16:15:21 -0300

    Layout base das páginas

--- cidadeiluminada/postes/rotas.py
+++ cidadeiluminada/postes/rotas.py
@@ -9,3 +9,8 @@ bp = Blueprint('postes', __name__, template_folder='templates',
 
 def init_app(app, url_prefix='/postes'):
     app.register_blueprint(bp, url_prefix=url_prefix)
+
+
+@bp.route('/')
+def index():
+    return render_template('index_poste.html')
