8ae76eb188f05e37164436a4d74a61d2d0bd54b4
Arthur Bressan <arthurpbressan@gmail.com>
2015-09-20 17:12:37 -0300

    Tentativa de usar o flask-admin

--- cidadeiluminada/postes/__init__.py
+++ cidadeiluminada/postes/__init__.py
@@ -7,6 +7,8 @@ from cidadeiluminada.postes import models, rotas  # NOQA
 
 
 def init_app(app):
-    admin = Admin(app, endpoint='postes', template_mode='bootstrap3', name='Postes', url='/postes')
+    admin = Admin(app, endpoint='postes', template_mode='bootstrap3',
+                  name='Postes', url='/postes',
+                  base_template='base_poste.html',)
     for view in rotas.init_app(app):
         admin.add_view(view)
