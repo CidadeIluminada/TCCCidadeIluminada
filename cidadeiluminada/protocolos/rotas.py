525f9fcf047e20acc123158d320890a84bc307bf
Arthur Bressan <arthurpbressan@gmail.com>
2015-10-11 14:20:51 -0300

    Começando o admin do 0

--- cidadeiluminada/protocolos/rotas.py
+++ cidadeiluminada/protocolos/rotas.py
@@ -1,75 +1,124 @@
 # coding: UTF-8
-# from __future__ import absolute_import
+from __future__ import absolute_import
 
-# from flask import request, jsonify
-# from flask.ext.admin import Admin, expose, AdminIndexView
-# from flask.ext.admin.contrib.sqla import ModelView
+from flask import request, jsonify
+from flask.ext.admin import Admin, expose, AdminIndexView
+from flask.ext.admin.contrib.sqla import ModelView
 
-# from cidadeiluminada.protocolos.models import Poste, Pendencia, ZonaCidade, Bairro, OrdemServico, \
-#     Rua
-# from cidadeiluminada.base import db
+from cidadeiluminada.protocolos.models import Regiao, Bairro, Logradouro, \
+    Poste, ItemManutencao, Protocolo, OrdemServico, ItemManutencaoOrdemServico
+from cidadeiluminada.base import db
 
-# _endereco_widget_args = {
-#     'estado': {
-#         'readonly': True,
-#     },
-#     'cidade': {
-#         'readonly': True,
-#     }
-# }
+_endereco_widget_args = {
+    'estado': {
+        'readonly': True,
+    },
+    'cidade': {
+        'readonly': True,
+    }
+}
 
-# _endereco_args = {
-#     'estado': {
-#         'default': u'SP',
-#     },
-#     'cidade': {
-#         'default': u'São José dos Campos',
-#     }
-# }
-
-
-# class IndexView(AdminIndexView):
-
-#     @expose('/')
-#     def index(self):
-#         pendencias_acao = Pendencia.query.filter(Pendencia.poste == None)
-#         return self.render('admin/index_postes.html', pendencias_acao=pendencias_acao)
-
-#     @expose('/ordem_servico/nova/')
-#     def ordem_servico(self):
-#         zonas = ZonaCidade.query.all()
-#         return self.render('admin/index_postes.html', zonas=zonas)
+_endereco_args = {
+    'estado': {
+        'default': u'SP',
+    },
+    'cidade': {
+        'default': u'São José dos Campos',
+    }
+}
 
 
-# class _ModelView(ModelView):
+class IndexView(AdminIndexView):
 
-#     category = None
+    @expose('/')
+    def index(self):
+        return super(IndexView, self).index()
+        # pendencias_acao = Pendencia.query.filter(Pendencia.poste == None)
+        # return self.render('admin/index_postes.html', pendencias_acao=pendencias_acao)
 
-#     def __init__(self, *args, **kwargs):
-#         kwargs.setdefault('name', self.name)
-#         kwargs.setdefault('category', self.category)
-#         super(_ModelView, self).__init__(self.model, db.session, *args, **kwargs)
+    # @expose('/ordem_servico/nova/')
+    # def ordem_servico(self):
+    #     regioes = Regiao.query.all()
+    #     return self.render('admin/index_postes.html', regioes=regioes)
 
 
-# class PosteView(_ModelView):
-#     model = Poste
-#     name = 'Postes'
-#     category = 'Protocolos'
+class _ModelView(ModelView):
 
-#     # form_widget_args = _endereco_widget_args
-#     # form_args = _endereco_args
-#     # form_columns = ('cep', 'numero', 'logradouro', 'bairro', 'cidade', 'estado')
-#     # form_excluded_columns = ('pendencias',)
+    category = None
 
-#     create_template = 'admin/model/edit_modelo_cep.html'
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('name', self.name)
+        kwargs.setdefault('category', self.category)
+        super(_ModelView, self).__init__(self.model, db.session, *args, **kwargs)
 
-#     @expose('/new/', methods=('GET', 'POST'))
-#     def create_view(self):
-#         bairros = Bairro.query
-#         bairro_id_nome = {bairro.nome: bairro.id for bairro in bairros}
-#         self._template_args['bairro_id_nome_map'] = bairro_id_nome
-#         return super(PosteView, self).create_view()
 
+class RegiaoView(_ModelView):
+    model = Regiao
+    name = u'Região'
+    category = u'Endereço'
+
+
+class BairroView(_ModelView):
+    model = Bairro
+    name = 'Bairro'
+    category = u'Endereço'
+
+    form_columns = ('regiao', 'nome')
+
+
+class LogradouroView(_ModelView):
+    model = Logradouro
+    name = 'Logradouro'
+    category = u'Endereço'
+
+    # create_template = 'admin/model/edit_modelo_cep.html'
+
+    # @expose('/new/', methods=('GET', 'POST'))
+    # def create_view(self):
+    #     bairros = Bairro.query
+    #     bairro_id_nome = {bairro.nome: bairro.id for bairro in bairros}
+    #     self._template_args['bairro_id_nome_map'] = bairro_id_nome
+    #     return super(RuaView, self).create_view()
+
+
+class PosteView(_ModelView):
+    model = Poste
+    name = 'Postes'
+    category = 'Protocolos'
+
+    # form_widget_args = _endereco_widget_args
+    # form_args = _endereco_args
+    # form_columns = ('cep', 'numero', 'logradouro', 'bairro', 'cidade', 'estado')
+    # form_excluded_columns = ('pendencias',)
+
+
+class ProtocoloView(_ModelView):
+    model = Protocolo
+    name = 'Protocolos 156'
+    category = 'Protocolos'
+
+
+class ItemManutencaoView(_ModelView):
+    model = ItemManutencao
+    name = u'Itens Manutenção'
+    category = 'Protocolos'
+
+
+class OrdemServicoView(_ModelView):
+    model = OrdemServico
+    name = u'Ordem de Serviço'
+    category = 'Protocolos'
+
+    form_excluded_columns = ('protocolos')
+
+    edit_template = 'admin/model/edit_os.html'
+
+    form_widget_args = {
+        'criacao': {
+            'readonly': True,
+            'disabled': True,
+        },
+    }
 
 # class PendenciaView(_ModelView):
 #     model = Pendencia
@@ -126,53 +175,22 @@
 #         return jsonify(payload={'status': 'ERRO', 'erros': form.errors}), 400
 
 
-# class ZonaCidadeView(_ModelView):
-#     model = ZonaCidade
-#     name = 'Zona'
-#     category = u'Endereço'
-
-
-# class BairroView(_ModelView):
-#     model = Bairro
-#     name = 'Bairro'
-#     category = u'Endereço'
-
-#     form_columns = ('zona', 'nome')
-
-
-# class RuaView(_ModelView):
-#     model = Rua
-#     name = 'Rua'
-#     category = u'Endereço'
-
-
-# class OrdemServicoView(_ModelView):
-#     model = OrdemServico
-#     name = u'Ordem de Serviço'
-#     category = 'Protocolos'
-
-#     form_excluded_columns = ('protocolos')
-
-#     edit_template = 'admin/model/edit_os.html'
-
-#     form_widget_args = {
-#         'criacao': {
-#             'readonly': True,
-#             'disabled': True,
-#         },
-#     }
-
-
 def init_app(app):
-    pass
-    # admin_config = {
-    #     'endpoint': 'protocolos',
-    #     'url': '/',
-    # }
-    # views = [PosteView(), PendenciaView(), ZonaCidadeView(), BairroView(), OrdemServicoView(),
-    #          RuaView()]
-    # index = IndexView(name='Principal', **config)
-    # admin = Admin(app, template_mode='bootstrap3', index_view=index, name='',
-    #               **admin_config)
-    # for view in views:
-    #     admin.add_view(view)
+    config = {
+        'endpoint': 'protocolos',
+        'url': '/',
+    }
+    views = [
+        RegiaoView(),
+        BairroView(),
+        LogradouroView(),
+        PosteView(),
+        ItemManutencaoView(),
+        ProtocoloView(),
+        OrdemServicoView(),
+    ]
+    index = IndexView(name='Principal', **config)
+    admin = Admin(app, template_mode='bootstrap3', index_view=index, name='Protocolos',
+                  **config)
+    for view in views:
+        admin.add_view(view)
