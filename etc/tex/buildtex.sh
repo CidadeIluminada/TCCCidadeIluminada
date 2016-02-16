171446f37bb5714c75031cd8c5b4c9a6cc6425b4
Arthur Bressan <arthurpbressan@gmail.com>
2016-02-15 23:24:45 -0200

    Corrige o comando rm & reformata o .tex
    Jurava que funcionava antes

--- etc/tex/buildtex.sh
+++ etc/tex/buildtex.sh
@@ -4,5 +4,12 @@ bibtex cidadeiluminada.aux
 makeindex cidadeiluminada.idx
 pdflatex cidadeiluminada.tex
 pdflatex cidadeiluminada.tex
-rm  !(*.tex|*.bib|*.sh|*.pdf|android|site)
+rm cidadeiluminada.bbl
+rm cidadeiluminada.blg
+rm cidadeiluminada.idx
+rm cidadeiluminada.ind
+rm cidadeiluminada.aux
+rm cidadeiluminada.brf
+rm cidadeiluminada.ilg
+rm cidadeiluminada.log
 evince cidadeiluminada.pdf &
